1bc1a2437ddd7458e020ef8997aa710fda40ccc7
Muhammad Qayyum <msqgrewal123@gmail.com>
2023-08-07 20:04:21 -0500

    App file is updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import { useState } from "react";
+import satDate from "./components/satData"
+
 
 
 function App() {
+
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
+
+const filterByType = (currentType) => {
+  const displaySats = satData.filter((newSatDisplay) => {
+     return newSatDisplay.orbitType === currentType;
+  });
+  setSat(displaySats);
+};
 
 export default App;
